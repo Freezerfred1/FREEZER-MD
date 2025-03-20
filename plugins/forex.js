3981015e403884f4c522401c62e3ba8054f26638
Freezerfred1 <freezermtoxic@gmail.com>
2025-03-20 19:34:09 +0300

    Update forex.js

--- plugins/forex.js
+++ plugins/forex.js
@@ -35,7 +35,7 @@ zokou({
     _0x556c57[_0x525567.categorie].push(_0x525567.nomCom);
   });
   moment.tz.setDefault('Asia/Karachi');
-  let _0x2313df = "\n╭────〖 *XGAGA-BOTS* 〗────╮\n│﹄ *Prefix* : " + s.PREFIXE + "\n│﹄ *Owner* : " + s.OWNER_NAME + "\n│﹄ *Mode* : " + _0x3a755b + "\n│﹄ *Commands* : " + _0xd029dc.length + " \n│﹄ *Ram* : " + format(os.totalmem() - os.freemem()) + '/' + format(os.totalmem()) + "\n│﹄ *Developer* : Gaga\n│﹄ *Version* : freeyond\n╰─────{ *XGAGA BOTS* }─────o: \n\n";
+  let _0x2313df = "\n╭────〖 *FREEZER-BOTS* 〗────╮\n│﹄ *Prefix* : " + s.PREFIXE + "\n│﹄ *Owner* : " + s.OWNER_NAME + "\n│﹄ *Mode* : " + _0x3a755b + "\n│﹄ *Commands* : " + _0xd029dc.length + " \n│﹄ *Ram* : " + format(os.totalmem() - os.freemem()) + '/' + format(os.totalmem()) + "\n│﹄ *Developer* : Gaga\n│﹄ *Version* : freeyond\n╰─────{ *FREEZER BOTS* }─────o: \n\n";
   let _0x7a5179 = "  \n\n*GAGA.BOTZ Commands :*\n◇                             ◇\n";
   for (const _0x7b5d9 in _0x556c57) {
     _0x7a5179 += "*o:* *" + _0x7b5d9 + "* *o:*";
@@ -44,7 +44,7 @@ zokou({
     }
     _0x7a5179 += "\n*╰═════════════⊷* \n";
   }
-  _0x7a5179 += "\n\n*—— Channel link: ——*\n\nhttps://whatsapp.com/channel/0029VasnifMFi8xW4Mqysn2F\n  \n*——————————————————————————————*\n";
+  _0x7a5179 += "\n\n*—— Channel link: ——*\n\nhttps://whatsapp.com/channel/0029Vb5mxL6F1YlTPss80Y2t\n  \n*——————————————————————————————*\n";
   var _0x2f3fd2 = _0x553074();
   if (_0x2f3fd2.match(/\.(mp4|gif)$/i)) {
     try {
@@ -53,7 +53,7 @@ zokou({
           'url': "https://files.catbox.moe/sfk02i.jpg"
         },
         'caption': _0x2313df + _0x7a5179,
-        'footer': "*Powered by Xgaga bots*",
+        'footer': "*Powered by FREEZER-MD bots*",
         'gifPlayback': true
       }, {
         'quoted': _0x170501
@@ -70,7 +70,7 @@ zokou({
             'url': _0x2f3fd2
           },
           'caption': _0x2313df + _0x7a5179,
-          'footer': "*XGAGA BOTS*"
+          'footer': "*FREEZER-MD BOTS*"
         }, {
           'quoted': _0x170501
         });
